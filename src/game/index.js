bc419a081904c42b7ee4e8c63d599c8fb2a2e152
Jesús Escamilla <jesusescamilla@hotmail.com>
2017-06-14 20:04:20 -0500

    first whater sqare

--- src/game/index.js
+++ src/game/index.js
@@ -16,11 +16,22 @@ function test() {
   game.context.fill();
 }
 
+function drawRectangle({ x, y, width, height }, background) {
+  game.context.fillStyle = background;
+  game.context.fillRect(x, y, width, height);
+}
+
+function drawWather() {
+  drawRectangle({ x: 0, y: 0, width: 100, height: 200 }, '#3cbff0')
+}
+
+
+
 function startGame() {
   game.canvas = addRef('canvas');
   game.context = game.canvas.getContext('2d');
   clear();
-  test();
+  drawWather();
 }
 
 export default startGame;
