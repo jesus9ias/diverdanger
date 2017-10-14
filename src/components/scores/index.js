4366f643f50cb2ac83f5f23935b462303d08fab3
Jesús Escamilla <jesusescamilla@hotmail.com>
2017-10-13 23:50:59 -0500

    some style to end game dialog

--- src/components/scores/index.js
+++ src/components/scores/index.js
@@ -2,14 +2,14 @@ import { appendElement } from '../../common/global';
 
 const Scores = (seconds, oxygen, life, energy, points, level) => {
   appendElement('dialogs', `<div id="scores" class="scores">
-    <h2>Scores</h2>
-    <p>Reached Level: ${level}</p>
-    <p>Points: ${points}</p>
-    <p>Game time: ${seconds}</p>
-    <p>Oxygen: ${oxygen.toFixed(2)}</p>
-    <p>Life: ${life}</p>
-    <p>Energy: ${energy}</p>
-    <p>Press r to restart</p>
+    <h2 class="dialog__title">Scores</h2>
+    <p class="dialog__text">Reached Level: ${level}</p>
+    <p class="dialog__text">Points: ${points}</p>
+    <p class="dialog__text">Game time: ${seconds}</p>
+    <p class="dialog__text">Oxygen: ${oxygen.toFixed(2)}</p>
+    <p class="dialog__text">Life: ${life}</p>
+    <p class="dialog__text">Energy: ${energy}</p>
+    <p class="dialog__text">Press r to restart</p>
   </div>`);
 };
 
